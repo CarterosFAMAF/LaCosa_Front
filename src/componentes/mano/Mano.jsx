3b5dc843303e29caf979684ce46a747c353806ed
Narayan <prema.narayan.ochoa@mi.unc.edu.ar>
2023-09-20 18:51:33 -0300

    Selección de Cartas

--- src/componentes/mano/Mano.jsx
+++ src/componentes/mano/Mano.jsx
@@ -1,13 +1,19 @@
 import './Mano.css'
 import Carta from "../carta/Carta";
+import { useState } from 'react';
 
 function Mano({imagenes_cartas}) {
     const output = [];
+    const [seleccion, setSeleccion] = useState(imagenes_cartas.map(_ => 0))
+    
+    const select_card = (id_card) => {
+        setSeleccion(seleccion.map((_, i) => (i === id_card)? 1 : 0));
+    }
 
     imagenes_cartas.forEach((imagen_carta, i) => {
         output.push(
             <li key={i}>
-                <Carta imagen={imagen_carta} id={i}></Carta>
+                <Carta select_card_method={select_card} imagen={imagen_carta} id={i} brillo={seleccion[i]}></Carta>
             </li>
         );
     });
