5d22db27f496ed9ecd64645e4c3ebee59e339710
Mateo Malpassi <mateo.malpass@mi.unc.edu.ar>
2023-09-22 20:19:20 -0300

    Formulario ingresar nombre

---
+++ src/componentes/formularioNombre/Formulario.jsx
@@ -0,0 +1,28 @@
+import {useForm} from 'react-hook-form'
+
+const Formulario = () => {
+    const {register, formState: {errors}, handleSubmit} = useForm();
+
+    const onSubmit = (data, e) => {
+        console.log(data)
+        e.target.reset();
+    }
+
+    return (
+        <div>
+            <h1>Ingrese su nombre de jugador</h1>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div>
+                        <label>Nombre</label>
+                        <input type="text" {...register('Nombre', {
+                            required: true
+                        })}/>
+                        {errors.Nombre && <p>El campo Nombre es requerido</p>}
+                    </div>
+                    <input type="submit" value="Enviar"/>
+                </form>
+        </div>
+    )
+}
+
+export default Formulario
