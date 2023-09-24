2d600970fb10fb15baedabfaee408832a57d34f1
Mateo Malpassi <mateo.malpass@mi.unc.edu.ar>
2023-09-24 18:11:55 -0300

    Agrego componente Home

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Formulario from './componentes/formularioNombre/Formulario.jsx'
+import Home from './componentes/home/Home.jsx'
 
 function App() {
+
+  const [nombre, setNombre] = useState('')
+
   return (
     <div>
-      <Formulario />
+      {
+        !nombre.length > 0 
+        ? <Formulario setNombre={setNombre} />
+        : <Home/>
+      }
     </div>
   )
 }
--- src/componentes/formularioNombre/Formulario.jsx
+++ src/componentes/formularioNombre/Formulario.jsx
@@ -1,26 +1,28 @@
-import {useForm} from 'react-hook-form'
+import {set, useForm} from 'react-hook-form'
 
-const Formulario = () => {
+const Formulario = ({setNombre}) => {
     const {register, formState: {errors}, handleSubmit} = useForm();
 
     const onSubmit = (data, e) => {
         console.log(data)
         e.target.reset();
+
+        setNombre(data.Nombre)
     }
 
     return (
         <div>
             <h1>Ingrese su nombre de jugador</h1>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div>
-                        <label>Nombre</label>
-                        <input type="text" {...register('Nombre', {
-                            required: true
-                        })}/>
-                        {errors.Nombre && <p>El campo Nombre es requerido</p>}
-                    </div>
-                    <input type="submit" value="Enviar"/>
-                </form>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label>Nombre</label>
+                    <input type="text" {...register('Nombre', {
+                        required: true
+                    })}/>
+                    {errors.Nombre && <p>El campo Nombre es requerido</p>}
+                </div>
+                <input type="submit" value="Enviar"/>
+            </form>
         </div>
     )
 }
