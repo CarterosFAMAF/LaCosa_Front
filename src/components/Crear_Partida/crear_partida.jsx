76f2bbafbd3432424c373b4d9488696d089bdb90
FacuOta <facu_otamendi@mi.unc.edu.ar>
2023-09-29 11:45:38 -0300

    Envio informacion a back con axios

--- src/components/Crear_Partida/crear_partida.jsx
+++ src/components/Crear_Partida/crear_partida.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import axios from "axios";
 import {
   TextField,
   Button,
@@ -18,7 +19,7 @@ const Crear_partida = () => {
       nombre_partida : '',
       nro_min_jug : 4,
       nro_max_jug : 12,
-      contra : ''
+      //contra : ''
     });
 
     const handleChange = (event) => {
@@ -26,9 +27,40 @@ const Crear_partida = () => {
     };
 
     const handleSubmit = (event) => {
-      alert("creaste una partida");
-    };
+      var bodyFormData = new FormData();
 
+      if (partida.nombre_jugador === '' || partida.nombre_partida === '' || partida.nro_min_jug === '' || partida.nro_max_jug === '') {
+        alert("All fields are required");
+      }
+
+      bodyFormData.append('player_name', partida.nombre_jugador);
+      bodyFormData.append('match_name', partida.nombre_partida);
+      bodyFormData.append('min_players', partida.nro_min_jug);
+      bodyFormData.append('max_players', partida.nro_max_jug);
+      
+      /*
+      if(partida.contra != ''){
+        bodyFormData.append('contra', 'contra');
+      }
+      */
+
+      axios({
+        method: 'post',
+        url:  'http://127.0.0.1:8000/matchs',
+        data: bodyFormData,
+        headers: { "Content-Type": "multipart/form-data" },
+
+      })
+      .then(function (response) {
+        //handle success
+        console.log(response);
+        alert(`Se creo la partida con exito. Su ID de usuario es, ${response.data.player_id} y su ID de partida ${response.data.match_id}` );
+      })
+      .catch(function (response) {
+        //handle error
+        alert(`error: ${response.message}`);
+      });
+    };
 
     return ( 
     <Container>
@@ -42,6 +74,7 @@ const Crear_partida = () => {
             variant="outlined"
             type = "Text"
             onChange={handleChange}/>
+        {/* Contraseña
         <TextField
             label= "Contraseña"
             name = 'contra'
@@ -49,6 +82,7 @@ const Crear_partida = () => {
             fullWidth
             type = "Text"
             onChange={handleChange}/>
+        */}
         <TextField
             label= "nombre_jugador"
             name = 'nombre_jugador'
