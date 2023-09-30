b501e6ca70ef022af5e06f21ce750f8cb0649794
Narayan <prema.narayan.ochoa@mi.unc.edu.ar>
2023-09-30 13:01:28 -0300

    posición de carpeta tests

--- src/componentes/tests/unir_jugador.test.jsx
+++ src/componentes/tests/unir_jugador.test.jsx
@@ -1,12 +1,11 @@
 import TestRenderer from 'react-test-renderer';
 import { describe, expect, test } from 'vitest'
-import UnirJugador from '../componentes/unir_Jugador/unir_jugador'
+import UnirJugador from '../unir_Jugador/unir_jugador';
 
 describe("Unir Jugador Test", () => {
 
     test("Renderiza Unir Jugador", () => {
         const elem = TestRenderer.create(<UnirJugador></UnirJugador>).toJSON()
-        console.log(elem)
         expect(elem).not.toBeNull
     })
 
