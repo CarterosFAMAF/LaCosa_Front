da45fac2129e339eb98b61c695ec9eb3fd0d530d
Narayan <prema.narayan.ochoa@mi.unc.edu.ar>
2023-09-30 17:31:40 -0300

    Mecánica de turno

--- src/componentes/jugador/jugar/JugarCarta.jsx
+++ src/componentes/jugador/jugar/JugarCarta.jsx
@@ -1,17 +1,17 @@
 import './JugarCarta.css'
 
-function JugarCarta({id_carta, setEsTurno, setCartas}) {
+function JugarCarta({id_carta, setTurno, setCartas}) {
 
     const jugar_carta = (id_carta) => {
         console.log(`Hacer petición al back de jugar la carta ${id_carta}`)
         setCartas(oldArray => (oldArray.filter(item => item.id !== id_carta)));
-        setEsTurno(false)
+        setTurno(turnoViejo => turnoViejo+1)
     }
 
     const descartar_carta = (id_carta) => {
         console.log(`Hacer petición al back de descartar la carta ${id_carta}`)
         setCartas(oldArray => (oldArray.filter(item => item.id !== id_carta)));
-        setEsTurno(false)
+        setTurno(turnoViejo => turnoViejo+1)
     }
 
     return (
