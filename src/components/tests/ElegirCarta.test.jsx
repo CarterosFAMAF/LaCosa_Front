d5372130b8a795a14a003507105388ac2f6368c5
FacuOta <facu_otamendi@mi.unc.edu.ar>
2023-11-13 10:53:30 -0300

    Tests

--- src/components/tests/ElegirCarta.test.jsx
+++ src/components/tests/ElegirCarta.test.jsx
@@ -1,60 +1,174 @@
-import TestRenderer from "react-test-renderer";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
 import ElegirCarta from "../partida/elegir_carta/ElegirCarta";
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, afterEach, vi } from "vitest";
+import {render, screen, cleanup } from '@testing-library/react';
+import {userEvent} from "@testing-library/user-event"
+import axios from 'axios';
+
+
+const generateInitialState = (customCartas, fase, opcionesDefensivas) => ({
+  jugador: {
+    cartas: customCartas,
+    opcionesDefensivas : opcionesDefensivas,
+    turnoPartida: 0,
+    fase: fase,
+    partidaId: 2,
+    jugadorid: 1,
+    seleccion: 1,
+    seleccionType: "Accion",
+    jugadores: [
+      { id: 5, name: 'Ernesto', turn: 0, alive: true },
+      { id: 6, name: 'Gustavo', turn: 1, alive: true },
+      { id: 8, name: 'Juan Carlos', turn: 2, alive: true },
+      { id: 7, name: 'Elena', turn: 3, alive: true },
+    ],
+  },
+  fase: {
+    robo: 1,
+    juego: 2,
+    objetivo: 3,
+    defensa: 4,
+    resultado: 5,
+    intercambio: 6,
+  },
+  typecard: {
+    accion: "Accion",
+    defensa: "Defensa",
+    panico: "Panico",
+    lacosa: "La_Cosa",
+    infectado: "Infectado",
+  },
+  rol: {
+    lacosa: "La_Cosa",
+    humano: "Humano",
+    infectado: "Infectado",
+  },
+});
 
 const mockStore = configureStore([]);
 
 describe("ElegirCarta Test", () => {
-  test("Renderiza ElegirCarta", () => {
-    const initialState = {
-      jugador: {
-        cartas: [
-          { id: 1, name: "lanzallamas", image:"a", type: "accion" },
-          { id: 2, name: "Mas_Vale_Que_Corras", image:"a", type: "accion"},
-          { id: 3, name: "Sospecha", image:"a", type : "accion" },
-        ],
-        turnoPartida: 0,
-        fase: 1,
-        seleccion: 1,
-        jugadores: [
-          { id: 5, name: 'Ernesto', turn: 0, alive: true },
-          { id: 6, name: 'Gustavo', turn: 1, alive: true },
-          { id: 8, name: 'Juan Carlos', turn: 2, alive: true },
-          { id: 7, name: 'Elena', turn: 3, alive: true },
-        ],
-      },
-      fase: {
-        robo: 0,
-        juego: 1,
-        objetivo: 2,
-        defensa: 3,
-        resultado: 4,
-        intercambio: 5,
-      },
-      typecard: {
-        accion: "Accion",
-        defensa: "Defensa",
-        panico: "Panico",
-        lacosa: "La_Cosa",
-        infectado: "Infectado",
-      },
-      rol: {
-        lacosa: "La_Cosa",
-        humano: "Humano",
-        infectado: "Infectado",
-      }
-    };
 
+  afterEach(() => {
+    vi.clearAllMocks();
+    vi.resetAllMocks();
+    cleanup();
+  })
+
+  test("Fase 2 (Juego) Con carta de acción", async () => {
+
+    const initialState = generateInitialState([
+      { id: 1, name: "lanzallamas", image:"a", type: "accion" },
+      { id: 2, name: "Mas_Vale_Que_Corras", image:"a", type: "accion"},
+      { id: 3, name: "Sospecha", image:"a", type : "accion" },
+    ], 2, []);
     const store = mockStore(initialState);
 
-    const elem = TestRenderer.create(
+    render(
       <Provider store={store}>
         <ElegirCarta />
       </Provider>
-    ).toJSON();
+    );
+    
+    const botonJugar = screen.queryByRole("button", {name: "Jugar"});
+    const botonDescartar = screen.queryByRole("button", {name: "Descartar"});
 
-    expect(elem).not.toBeNull();
+    expect(botonJugar).toBeTruthy();
+    expect(botonDescartar).toBeTruthy();
+
+    await userEvent.click(botonJugar);
+    
+    expect(screen.queryByRole("button")).toBeFalsy();
   });
+
+  test("Fase 2 (Juego) Con carta de Panico", async () => {
+
+    const initialState = generateInitialState([
+      { id: 1, name: "panico", image:"a", type: "Panico" },
+      { id: 2, name: "Mas_Vale_Que_Corras", image:"a", type: "accion"},
+      { id: 3, name: "Sospecha", image:"a", type : "accion" },
+    ], 2, []);
+    const store = mockStore(initialState);
+
+    render(
+      <Provider store={store}>
+        <ElegirCarta />
+      </Provider>
+    );
+    
+    const botonPanico = screen.queryByRole("button", {name: "Pánico!!!!"});
+
+    expect(botonPanico).toBeTruthy();
+
+    await userEvent.click(botonPanico);
+
+    expect(screen.queryByRole("button")).toBeFalsy();
+
+  });
+
+  test("Fase 4 (defensa)", async () => {
+    vi.mock('axios');
+    axios.put.mockResolvedValue({data: {}})
+    axios.get.mockResolvedValue({
+      data: {
+        respuesta: "random"
+      }
+    });
+
+    const initialState = generateInitialState([
+      { id: 1, name: "Defensa", image:"a", type: "Defensa" },
+      { id: 2, name: "Mas_Vale_Que_Corras", image:"a", type: "accion"},
+      { id: 3, name: "Sospecha", image:"a", type : "accion" },
+    ], 4, [1]);
+    const store = mockStore(initialState);
+
+    render(
+      <Provider store={store}>
+        <ElegirCarta />
+      </Provider>
+    );
+    
+    const botonNoDefender = screen.queryByRole("button", {name: "No Defender"});
+    expect(botonNoDefender).toBeTruthy();
+
+
+    const botonDefender = screen.queryByRole("button", {name: "Defender"});
+    expect(botonDefender).toBeTruthy();
+
+    await userEvent.click(botonDefender);
+
+    expect(screen.queryByRole("button")).toBeFalsy();
+  });
+
+  test("Fase 3 (objetivo)", async () => {
+    vi.mock('axios');
+    axios.put.mockResolvedValue({data: {}})
+
+    const initialState = generateInitialState([
+      { id: 1, name: "lanzallamas", image:"a", type: "accion" },
+      { id: 2, name: "Mas_Vale_Que_Corras", image:"a", type: "accion"},
+      { id: 3, name: "Sospecha", image:"a", type : "accion" },
+    ], 3, []);
+    const store = mockStore(initialState);
+
+    render(
+      <Provider store={store}>
+        <ElegirCarta />
+      </Provider>
+    );
+    
+    const botonAdyacente1 = screen.queryByRole("button", {name: "Gustavo"});
+    expect(botonAdyacente1).toBeTruthy();
+
+
+    const botonAdyacente2 = screen.queryByRole("button", {name: "Elena"});
+    expect(botonAdyacente2).toBeTruthy();
+
+    await userEvent.click(botonAdyacente1);
+
+    expect(screen.queryByRole("button")).toBeFalsy();
+  });
+
+
 });
--- src/components/tests/Jugador.test.jsx
+++ src/components/tests/Jugador.test.jsx
@@ -1,21 +1,80 @@
-import TestRenderer from "react-test-renderer";
-import { Provider } from "react-redux";
-import { describe, expect, test } from "vitest";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Jugador from "../partida/Jugador";
 import store from "../../store/store"
 
+
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import { describe, expect, test, afterEach, vi } from "vitest";
+import {render, screen, cleanup } from '@testing-library/react';
+
+const initialState = {
+  jugador: {
+    partidaNombre: 'Fiesta',
+    partidaId: 3,
+    id: 2,
+    vivo: false,
+    iniciada: true,
+    rol:"Humano",
+    chat:["hola"]
+},
+rol: {
+  lacosa: "La_Cosa",
+  humano: "Humano",
+  infectado: "Infectado",
+}}
+const initialState1 = {
+  jugador: {
+    partidaNombre: 'Fiesta',
+    partidaId: 3,
+    id: 2,
+    vivo: true,
+    iniciada: true,
+    rol:"Humano",
+    chat:["hola"]
+  },
+  rol: {
+    lacosa: "La_Cosa",
+    humano: "Humano",
+    infectado: "Infectado",
+  },
+  fase: {
+    robo: 1,
+    juego: 2,
+    objetivo: 3,
+    defensa: 4,
+    resultado: 5,
+    intercambio: 6,
+}}
+
+const mockStore = configureStore([]);
+
 describe("Jugador Test", () => {
-  test("Renderiza Jugador", () => {
-    const elem = TestRenderer.create(
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={null} />
-          <Route path="/partida" element={Jugador} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>).toJSON();
-    expect(elem).not.toBeNull;
+/*  test("Muerto", () => {
+    const store = mockStore(initialState);
+
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Jugador/>
+        </BrowserRouter>
+      </Provider>);
+    console.log(screen.debug())
+    expect(screen.queryByText("Estás Muerto")).toBeTruthy()
+  });
+*/
+
+  test("Muerto", () => {
+    /*
+    const store = mockStore(initialState1);
+
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Jugador/>
+        </BrowserRouter>
+      </Provider>);
+    console.log(screen.debug())
+    expect(screen.queryByText("Estás Muerto")).toBeTruthy()*/
   });
 });
